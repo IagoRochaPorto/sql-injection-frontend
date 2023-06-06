ad911e551384d80937ffb9e6e352f23af0c430b6
Iago Rocha Porto <iagorochaporto@gmail.com>
2023-06-06 17:27:46 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+type Product = {
+  id: number
+  name: string
+  category: string
+  released: boolean
+}
+
+function App() {
+
+  const [products, setProducts] = useState<Product[]>([])
+
+  async function handleSubmit(category: string) {
+    const response = await fetch(`http:////172.12.169.108:3000?category=${category}`)
+    const data = await response.json()
+    setProducts(data)
+  }
+
+  return (
+    <>
+      <form action='get'>
+        <select name="category" id="1" defaultValue="" onChange={(event) => handleSubmit(event.target.value)}>
+          <option value="" disabled>Selecione uma categoria</option>
+          <option value="alimento">Alimento</option>
+          <option value="vestuário">Vestuário</option>
+        </select>
+      </form>
+
+      <div>
+        {products.map(product =>
+          <p key={product.id}>{product.name}</p>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default App
